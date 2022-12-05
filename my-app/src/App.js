9ba16f0ff7ba3527a8175c5b6e044f53e0d15762
ozen <nebilaozer@gmail.com>
2022-12-05 16:29:49 -0500

    Finished animation on initial welcome screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,10 +6,8 @@ function App() {
 // Create a <Title> react component that renders an <h1> which is
 // centered, palevioletred and sized at 1.5em
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
         <HelloScreen/>
-      </header>
     </div>
   );
 }
--- my-app/src/mods/HelloScreen.js
+++ my-app/src/mods/HelloScreen.js
@@ -1,21 +1,44 @@
 import React from "react";
-import "./HelloScreen.css"
+import "./HelloScreen.css";
+import {CSSTransition} from 'react-transition-group';
+
 
 function HelloScreen(props) {
 
-  const [button,setButton] = React.useState(true);
+  const [timeCounter,setTimeCounter] = React.useState(0);
+  const [buttonIn,setButtonIn] = React.useState(true);
 
   setTimeout(function () {
-    setButton(false);
-  }, 5000);
+    setTimeCounter((prevState) => {return prevState + 1});
+  }, 500);
+
+
+  const continueButtonHandler = () => {
+    setButtonIn(false);
+  }
 
   return (
-      <div>
-      <p>
-        Hello, My name is Nebil
-      </p>
-      {button ? null: <button className="button">continue</button>}
-      </div>
+    <div className="background">
+          <div className="text_div">
+          <CSSTransition
+            in = {((3 < timeCounter && 18 > timeCounter) || 21 < timeCounter) && buttonIn}
+            timeout = {500}
+            classNames="text"
+            unmountOnExit
+            >
+            <p className="text"> {timeCounter < 21 ? "Hello, My name is Nebil Ozer" : "Welcome to my humble abode!"}</p>
+          </CSSTransition>
+          <CSSTransition
+            in = {timeCounter > 22 && buttonIn}
+            timeout = {500}
+            classNames="button"
+            unmountOnExit
+            >
+            <button className="button" onClick={continueButtonHandler}>continue</button>
+          </CSSTransition>
+        </div>
+      
+    </div>
   );
 }
 
