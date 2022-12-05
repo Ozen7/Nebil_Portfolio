5e000842fe3c72143872ce04b3b8651cfc1c65b8
ozen <nebilaozer@gmail.com>
2022-12-05 16:49:45 -0500

    Finished up work with initial welcome screen

--- my-app/src/mods/HelloScreen.js
+++ my-app/src/mods/HelloScreen.js
@@ -1,45 +1,52 @@
 import React from "react";
 import "./HelloScreen.css";
-import {CSSTransition} from 'react-transition-group';
-
+import { CSSTransition } from "react-transition-group";
 
 function HelloScreen(props) {
-
-  const [timeCounter,setTimeCounter] = React.useState(0);
-  const [buttonIn,setButtonIn] = React.useState(true);
+  const [timeCounter, setTimeCounter] = React.useState(0);
+  const [buttonIn, setButtonIn] = React.useState(true);
 
   setTimeout(function () {
-    setTimeCounter((prevState) => {return prevState + 1});
+    setTimeCounter((prevState) => {
+      return prevState + 1;
+    });
   }, 500);
-
 
   const continueButtonHandler = () => {
     setButtonIn(false);
-  }
+  };
 
   return (
-    <div className="background">
-          <div className="text_div">
-          <CSSTransition
-            in = {((3 < timeCounter && 18 > timeCounter) || 21 < timeCounter) && buttonIn}
-            timeout = {500}
-            classNames="text"
-            unmountOnExit
-            >
-            <p className="text"> {timeCounter < 21 ? "Hello, My name is Nebil Ozer" : "Welcome to my humble abode!"}</p>
-          </CSSTransition>
-          <CSSTransition
-            in = {timeCounter > 22 && buttonIn}
-            timeout = {500}
-            classNames="button"
-            unmountOnExit
-            >
-            <button className="button" onClick={continueButtonHandler}>continue</button>
-          </CSSTransition>
-        </div>
-      
+    <div className="text_div">
+      <CSSTransition
+        in={
+          ((3 < timeCounter && 18 > timeCounter) || 21 < timeCounter) &&
+          buttonIn
+        }
+        timeout={500}
+        classNames="text"
+        unmountOnExit
+      >
+        <p className="text">
+          {" "}
+          {timeCounter < 21
+            ? "Hello, My name is Nebil Ozer"
+            : "Welcome to my humble abode!"}
+        </p>
+      </CSSTransition>
+
+      <CSSTransition
+        in={timeCounter > 22 && buttonIn}
+        timeout={500}
+        classNames="button"
+        unmountOnExit
+      >
+        <button className="button" onClick={continueButtonHandler}>
+          continue
+        </button>
+      </CSSTransition>
     </div>
   );
 }
 
-export default HelloScreen
+export default HelloScreen;
