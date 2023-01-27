514fb4990d5e6d5d2d877052a07561b43bc5e2d3
ozen <nebilaozer@gmail.com>
2023-01-26 19:32:35 -0500

    Work Experience Updated

--- Portfolio-Code/src/App.js
+++ Portfolio-Code/src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import HelloScreen from './mods/HelloScreen/HelloScreen';
-import {Routes, Route, BrowserRouter} from "react-router-dom";
 import HomePage from './mods/HomePage/HomePage';
 function App() {
 
@@ -8,14 +6,7 @@ function App() {
 // Create a <Title> react component that renders an <h1> which is
 // centered, palevioletred and sized at 1.5em
   return (
-    <BrowserRouter>
-      <div>
-          <Routes>
-            <Route path = "/HelloScreen" element = {<HelloScreen/>}/>
-            <Route path = "/" element = {<HomePage/>}/>
-          </Routes>
-      </div>
-    </BrowserRouter>
+    <HomePage/>
   );
 }
 
